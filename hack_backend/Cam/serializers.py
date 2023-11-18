7559693b677e89f236cecdb31cbe2810c0b5f508
Seth Fagin <sethfgn@gmail.com>
2023-11-18 11:51:41 -0500

    fixing unwanted files

--- hack_backend/Cam/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Cam
-
-class CamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cam
-        fields = ['id', 'pixelWidth', 'pixelLength', 'pixelSize']
--- hack_backend/Map/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Mat
-
-class MatSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mat
-        fields = ['id', 'matWidth', 'matLength', 'matSize']
