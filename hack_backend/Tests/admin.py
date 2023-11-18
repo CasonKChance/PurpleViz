aa22d89a3595bdd56b75e19668d4b0af505941f5
Seth Fagin <sethfgn@gmail.com>
2023-11-18 11:42:34 -0500

    reverting changes

--- hack_backend/Tests/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Tests
-
-# Register your models here.
-
-admin.site.register(Tests)
--- hack_backend/Tests/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Tests(models.Model):
-    testTotal = models.IntegerField()
-    testCount = models.IntegerField()
