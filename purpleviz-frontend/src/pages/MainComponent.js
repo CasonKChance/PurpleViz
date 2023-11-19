0581724120d52ba7b7d39b1bd9296849f426e043
Drake Griffith <Drakegriffith8@gmail.com>
2023-11-18 22:04:36 -0500

    Add files via upload

--- purpleviz-frontend/src/pages/MainComponent.js
+++ purpleviz-frontend/src/pages/MainComponent.js
@@ -4,40 +4,40 @@ import { nanoid } from 'nanoid';
 
 function MainComponent() {
   const [post, setPost] = useState({
-    testID: nanoid(),
-    toleranceLevel: 'tolerance',
+    toleranceLevel: '',
+    matWidth: '',
+    matLength: '',
   });
-  
-  const handleInputChange = (event) => {
-    setPost({ ...post, [event.target.name]: event.target.value });
-  };
   
   const handleSubmit = (event) => {
     event.preventDefault();
     axios.post('http://127.0.0.1:8000/api/Test/', post)
       .then(response => {
         console.log(response);
-        window.location.href = "http://localhost:3000/camera";
+        window.location.href = '/camera'
       })
       .catch(error => {
         console.log(error);
       });
   };
-  
-    return (
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100vh'}}>
-        <div style={{width: '50%', textAlign: 'center'}}>
-          <form onSubmit={handleSubmit}>
-         
-            <input type="text" name="sizeW" placeholder="Size (Width, in)" onChange={handleInputChange} style={{padding: '7.5px', margin: '5px'}}></input><br></br>
-            <input type="text" name="sizeL" placeholder="Size (Length, in)" onChange={handleInputChange} style={{padding: '7.5px', margin: '5px'}}></input><br></br>
-            <input type="text" name="tolerance" placeholder="Tolerance" onChange={handleInputChange} style={{padding: '7.5px', margin: '5px'}}></input><br></br>
-            <button onClick={handleSubmit} style={{width: '15%', marginLeft: 'auto', marginRight: 'auto', marginTop: '15px'}}>Submit</button>
-          </form>
-  
-        </div>
+
+  const handleInputChange = (event) => {
+    setPost({ ...post, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100vh' }}>
+      <div style={{ width: '50%', textAlign: 'center' }}>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="matWidth" placeholder="Size (Width, in)" onChange={handleInputChange} style={{ padding: '7.5px', margin: '5px' }} /><br />
+          <input type="text" name="matLength" placeholder="Size (Length, in)" onChange={handleInputChange} style={{ padding: '7.5px', margin: '5px' }} /><br />
+          <input type="text" name="toleranceLevel" placeholder="Tolerance" onChange={handleInputChange} style={{ padding: '7.5px', margin: '5px' }} /><br />
+          <button type="submit" style={{ width: '15%', marginLeft: 'auto', marginRight: 'auto', marginTop: '15px' }}>Submit</button>
+        </form>
       </div>
-    )}
+    </div>
+  );
+}
   
   
   export default MainComponent;
